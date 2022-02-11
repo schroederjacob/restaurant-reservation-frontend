6ed98093b510a048411dd498fd15d597d320f0b1
DougMellon <dm@dougmellon.com>
2022-02-10 17:54:52 -0700

    Added render code for Auth0. Commented out during development.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,13 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDOM.render(
+//     <Auth0Provider
+//         domain="YOUR_DOMAIN"
+//         clientId="YOUR_CLIENT_ID"
+//         redirectUri={window.location.origin}
+//     >
+//         <App />
+//     </Auth0Provider>,
+//     document.getElementById("root")
+// );
