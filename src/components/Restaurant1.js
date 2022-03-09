caed6c9cd33b33a4a1a2c7462061c24e129fd972
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2022-03-08 17:35:52 -0700

    Removed Search

--- src/components/Restaurant1.js
+++ src/components/Restaurant1.js
@@ -3,13 +3,41 @@ import { useNavigate } from "react-router-dom";
 
 
 
+// function onButtonClickHandler(){
+//     window.alert('You have selected this table for your reservation!')
+
+
 function Restaurant1(){
     let navigate = useNavigate();
+
+    // onButtonClickHandler = () => {
+    //     window.alert('You have selected this table for your reservation!')
+    //   };
+
+    
 
 
     return(
        <div class="square">
            <h1 style={{textAlign: 'center'}}>Taco Place</h1>
+
+           {/* <ul>
+               <li style={{textAlign: 'left'}}>Table 5</li>
+               <li style={{textAlign: 'left'}}><button>SELECT</button></li> 
+           </ul>
+           <ul>
+               <li style={{textAlign: 'left'}}>Table 6</li>
+               <li style={{textAlign: 'left'}}><button>SELECT</button></li> 
+           </ul>
+           <ul>
+               <li style={{textAlign: 'right'}}>Table 7</li>
+               <li style={{textAlign: 'right'}}><button>SELECT</button></li> 
+           </ul>
+           <ul>
+               <li style={{textAlign: 'right'}}>Table 8</li>
+               <li style={{textAlign: 'right'}}><button>SELECT</button></li> 
+           </ul> */}
+
            <ul>
                <li style={{textAlign: 'center'}}>Table 1</li>
                <li style={{textAlign: 'center'}}><button>SELECT</button></li> 
@@ -28,7 +56,6 @@ function Restaurant1(){
            </ul>
            <ul>
            <li><button onClick={() =>(navigate("/zipcode"))}>Back </button></li>
-
            </ul>
            
 
